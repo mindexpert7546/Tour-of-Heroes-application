3b9a78a466cc4eb06e9c3aca9a6ac526c11b3844
mindexpert7546 <Kundan.Kumar@iinterchange.net>
2024-02-19 15:09:33 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MessagesComponent,
+        DashboardComponent,
+       
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HeroesComponent
+    ]
 })
 export class AppModule { }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import {
+  NgFor,
+  NgIf,
+  UpperCasePipe,
+} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { RouterLink } from '@angular/router';
+
+@Component({
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrl: './heroes.component.css',
+    standalone: true,
+    imports: [
+        FormsModule,
+        NgIf,
+        NgFor,
+        UpperCasePipe,
+        HeroDetailComponent,
+        RouterLink
+    ]
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService:MessageService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+      res => {
+        console.log(res);
+        this.heroes = res;
+      }
+    )
+  }
+
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+}
+}
