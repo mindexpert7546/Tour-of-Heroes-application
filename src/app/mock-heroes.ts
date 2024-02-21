f507b80a5a5b9e7afaa88c1e43365be1c1ef8fa9
mindexpert7546 <Kundan.Kumar@iinterchange.net>
2024-02-21 18:20:52 +0530

    Message component html button color change

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from "./hero";
 
 export const HEROES: Hero[] =[
-  // { id: 12, heroes: 'Dr. Nice' },
-  // { id: 13, heroes: 'Bombasto' },
-  // { id: 14, heroes: 'Celeritas' },
-  // { id: 15, heroes: 'Magneta' },
-  // { id: 16, heroes: 'RubberMan' },
-  // { id: 17, heroes: 'Dynama' },
-  // { id: 18, heroes: 'Dr. IQ' },
-  // { id: 19, heroes: 'Magma' },
-  // { id: 20, heroes: 'Tornado' }
+  { id: 1, heroes: 'Dr. Nice' },
+  { id: 2, heroes: 'Bombasto' },
+  { id: 3, heroes: 'Celeritas' },
+  { id: 4, heroes: 'Magneta' },
+  { id: 5, heroes: 'RubberMan' },
+  { id: 6, heroes: 'Dynama' },
+  { id: 7, heroes: 'Dr. IQ' },
+  { id: 8, heroes: 'Magma' },
+  { id: 9, heroes: 'Tornado' }
 ];
